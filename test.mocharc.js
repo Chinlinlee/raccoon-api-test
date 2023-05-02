04ec3000451bd0865875891f688384a9e5246a12
chin <a5566qq2581@gmail.com>
2023-05-02 22:11:38 +0800

    chore: add `retrieveThumbnail` in config of file

--- test.mocharc.js
+++ test.mocharc.js
@@ -12,10 +12,11 @@ module.exports = {
         "test/WADO-RS/retrieveInstances.test.js",
         "test/WADO-RS/retrieveRendered.test.js",
         "test/WADO-RS/metadata.test.js",
+        "test/WADO-RS/retrieveThumbnail.test.js",
         "test/QIDO-RS/search.test.js",
         "test/DIMSE/store-instnace.test.js",
         "test/DIMSE/find.test.js",
-        "test/WADO-URI/retrieveInstance.test.js"
+        "test/WADO-URI/retrieveInstance.test.js",
     ],
     "sort": false
 }
