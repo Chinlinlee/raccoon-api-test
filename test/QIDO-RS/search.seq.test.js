0b1d9b69b9859c72337a9ca84dd61e2b391ac89f
chinlinlee <a5566qq2581@gmail.com>
2024-01-31 16:43:55 +0800

    fix: remove slash in the end of url

--- test/QIDO-RS/search.seq.test.js
+++ test/QIDO-RS/search.seq.test.js
@@ -14,7 +14,7 @@ const {storeInstance} = require("../../utils/storeInstance");
 class SeriesRequestAttributeSequenceParametersTester {
     testAccessionNumber() {
         it("should search AccessionNumber of Series Request Attribute Sequence (tag, 00400275.00080050) successful and return 1 series", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/series/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/series`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("00400275.00080050", studyCollection[5].RequestAttribute.AccessionNumber);
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
@@ -25,7 +25,7 @@ class SeriesRequestAttributeSequenceParametersTester {
         });
 
         it("should search not exist AccessionNumber of Series Request Attribute Sequence (tag, 00400275.00080050) successful", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/series/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/series`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("00400275.00080050", "foobar");
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
@@ -36,7 +36,7 @@ class SeriesRequestAttributeSequenceParametersTester {
 
     testIssuerLocalNamespaceEntityID() {
         it("should search LocalNamespaceEntityID of Issuer in Series Request Attribute Sequence (tag, 00400275.00080051.00400031) successful and return 1 series", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/series/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/series`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("00400275.00080051.00400031", studyCollection[5].RequestAttribute.Issuer.NamespaceEntityID);
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
@@ -47,7 +47,7 @@ class SeriesRequestAttributeSequenceParametersTester {
         });
 
         it("should search not exist LocalNamespaceEntityID of Issuer in Series Request Attribute Sequence (tag, 00400275.00080051.00400031) successful", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/series/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/series`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("00400275.00080051.00400031", "foo");
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
@@ -60,7 +60,7 @@ class SeriesRequestAttributeSequenceParametersTester {
 class InstancesConceptNameCodeParametersTester {
     testCodeValue() {
         it("should search CodeValue of Concept Name Code (tag, 0040A043.00080100) successful and return 1 series", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("0040A043.00080100", studyCollection[5].ConceptNameCode.CodeValue);
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
@@ -71,7 +71,7 @@ class InstancesConceptNameCodeParametersTester {
         });
 
         it("should search not exist CodeValue of Concept Name Code (tag, 0040A043.00080100) successful", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("0040A043.00080100", "foo");
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
@@ -84,7 +84,7 @@ class InstancesConceptNameCodeParametersTester {
 class InstancesContentParametersTester {
     testTextValue() {
         it("should search Text Value of Content (tag, 0040A730.0040A160) successful and return 1 series", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("0040A730.0040A160", studyCollection[5].Content.TextValue);
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
@@ -95,7 +95,7 @@ class InstancesContentParametersTester {
         });
 
         it("should search Text Value of Content (tag, 0040A730.0040A160) successful", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("0040A043.00080100", "foo");
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
@@ -105,7 +105,7 @@ class InstancesContentParametersTester {
     }
     testConceptNameCodeValue() {
         it("should search Code Value of Concept Name Code in Content (tag, 0040A730.0040A043.00080100) successful and return 1 series", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("0040A730.0040A043.00080100", studyCollection[5].Content.ConceptNameCode.CodeValue);
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
@@ -116,7 +116,7 @@ class InstancesContentParametersTester {
         });
 
         it("should search Code Value of Concept Name Code in Content (tag, 0040A730.0040A043.00080100) successful", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("0040A730.0040A043.00080100", "foo");
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
@@ -128,7 +128,7 @@ class InstancesContentParametersTester {
     testConceptCodeValue() {
         
         it("should search Code Value of Concept Code in Content (tag, 0040A730.0040A168.00080100) successful and return 1 series", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("0040A730.0040A168.00080100", studyCollection[5].Content.ConceptCode.CodeValue);
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
@@ -139,7 +139,7 @@ class InstancesContentParametersTester {
         });
 
         it("should search Code Value of Concept Code in Content (tag, 0040A730.0040A168.00080100) successful", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("0040A730.0040A168.00080100", "foo");
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
@@ -152,7 +152,7 @@ class InstancesContentParametersTester {
 class InstancesVerifyingObserverParametersTester {
     testVerifyingObserverName() {
         it("should search Observer Name of Verifying Observer (tag, 0040A073.0040A075) successful and return 1 series", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("0040A073.0040A075", studyCollection[5].VerifyingObserver.VerifyingObserverName);
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
@@ -163,7 +163,7 @@ class InstancesVerifyingObserverParametersTester {
         });
 
         it("should search not exist Observer Name of Verifying Observer (tag, 0040A073.0040A075) successful", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("0040A073.0040A075", "foo");
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
@@ -174,7 +174,7 @@ class InstancesVerifyingObserverParametersTester {
 
     testVerificationDateTime() {
         it("should search (-20201231) of Verification Date Time of Verifying Observer (tag, 0040A073.0040A030) successful and return 1 series", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("0040A073.0040A030", "-20201231");
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
@@ -185,7 +185,7 @@ class InstancesVerifyingObserverParametersTester {
         });
 
         it("should search Verification Date Time of Verifying Observer (tag, 0040A073.0040A030) successful and return 1 series", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("0040A073.0040A030", studyCollection[5].VerifyingObserver.VerificationDateTime);
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
@@ -196,7 +196,7 @@ class InstancesVerifyingObserverParametersTester {
         });
 
         it("should search not exist Verification Date Time of Verifying Observer (tag, 0040A073.0040A030) successful", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("0040A073.0040A030", "-19991212");
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
@@ -207,7 +207,7 @@ class InstancesVerifyingObserverParametersTester {
 
     testVerifyingOrganization() {
         it("should search Organization of Verifying Observer (tag, 0040A073.0040A027) successful and return 1 series", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("0040A073.0040A027", studyCollection[5].VerifyingObserver.VerifyingOrganization);
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
@@ -218,7 +218,7 @@ class InstancesVerifyingObserverParametersTester {
         });
 
         it("should search Organization of Verifying Observer (tag, 0040A073.0040A027) successful", async()=>{
-            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances/`, config.DICOMwebServer.baseUrl);
+            let searchURL = new URL(`${config.DICOMwebServer.qidoPrefix}/instances`, config.DICOMwebServer.baseUrl);
             searchURL.searchParams.append("0040A073.0040A027", "foo");
             let searchResponse = await axios.get(searchURL.href, {
                 headers: { 'Accept': 'application/dicom+json'}
